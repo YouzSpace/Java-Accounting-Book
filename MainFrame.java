3c4b1b09c4feb49dfea211e58f2715cd2d34e139
java设计 <2912808640@qq.com>
2025-06-24 08:51:32 +0800

    更新数据库表格

--- MainFrame.java
+++ MainFrame.java
@@ -7,6 +7,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class MainFrame extends JFrame {
 	JButton  addBT;
@@ -14,6 +17,11 @@ public class MainFrame extends JFrame {
 	JButton updateBT;
 	JButton  refreshBT;
 	JLabel bgimg;
+	JLabel bg;
+	// 表格数据
+	DefaultTableModel dm;
+	JTable tb;
+	JScrollPane scroll;
 	
 	
 	
@@ -45,6 +53,14 @@ public class MainFrame extends JFrame {
 		updateBT = new JButton("修改记录");
 		refreshBT = new JButton("刷新");
 		bgimg = new JLabel(new ImageIcon("img/1bg.jpg"));
+		// 表格标题
+		String[] title = {"ID","交易日期","交易类别","交易金额","交易描述"};
+		dm = new DefaultTableModel(title,0);
+		tb = new JTable();
+		tb.setModel(dm);
+		scroll = new JScrollPane(tb);
+				
+		
 	}
 	
 	public void initLayout() {
@@ -53,11 +69,13 @@ public class MainFrame extends JFrame {
 		delBT.setBounds(350, 60, 100, 40);
 		updateBT.setBounds(550, 60, 100, 40);
 		refreshBT.setBounds(750, 60, 100, 40);
+		scroll.setBounds(150, 140, 700, 400);
 		bgimg.setBounds(0, 0, 1000, 1000);
 		add(addBT);
 		add(delBT);
 		add(updateBT);
 		add(refreshBT);
+		add(scroll);
 		add(bgimg);
 	}
 	
